24cd3dac9abe58cb5fda70d1807113a13c6ca69d
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2023-07-25 08:34:42 +0900

    usersテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  devise_for :users
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
